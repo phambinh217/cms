ae30deb4c11ea65197c6eab8abbea3b30b2ba0b2
phambinh217 <phambinh217@gmail.com>
2017-03-24 10:28:28 +0700

    Fix bug undefined

--- src/Support/Traits/Author.php
+++ src/Support/Traits/Author.php
@@ -8,7 +8,7 @@ trait Author
 {
     public function setAuthorIdAttribute($value)
     {
-        if (User::where('id', $id)->exists()) {
+        if (User::where('id', $value)->exists()) {
             $this->attributes['author_id'] = $value;
         } else {
             $this->attributes['author_id'] = User::first()->id;
